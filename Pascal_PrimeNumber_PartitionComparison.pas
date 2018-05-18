34e8cd12a00dcb41ae867a91df7be0441927ee2e
AminaMois <abekina@gmail.com>
2018-05-18 20:33:35 +0300

    Program differs prime numbers form composite numbers and finds prime divisors of a number.

--- Pascal_PrimeNumber_PartitionComparison.pas
+++ Pascal_PrimeNumber_PartitionComparison.pas
@@ -1,113 +1,107 @@
 {
-This program reads text file and then prints on the screen words
-with no repeating from each row.
-}
-program ReadFromFile;
+1)???????????? ?????? ?????, ????????? ????? ?????????? ????????,
+	???? ??????? ???????.
+2)???????????? ?????? ??????? ?????,????????? ??????????, ??????? ??????
+3) ??????? ?????????? ? ?????? ??????? ???????? (?????)
+ ? ????? ?????? ?????????? ???? ??????? ?????????. ? ??????? 2 ????????? -
+ ????????? ????? ? ??? ???????? ??????}
+
+Program PartitionComparison;
 uses crt;
 
-{
-Array of string WordArray is used to store words got from
-the input string.
-}
+type MyArray= Array[1..100] of integer;
+var A:MyArray;
 
-type WordArray = array[1 .. 5000] of string;
 
-{
-Procedure GetWords writes content between 'space' into the array
-WordArray. Procedure GetWords returns number of words in the array.
-}
-
-//i - counter variable, used to check each character in the 
-//	  string, to check for 'space'
-//FirstLetterPosition - local variable, used to get content
-//						from the string, if it is not 'space'
-//WordCount - global variable, used to count words in the input string
-
-procedure GetWords(s:string; var words:WordArray; var WordCount:integer);
-var i, FirstLetterPosition:integer;
+{Function IsPrimeNumber differs prime number from a composite number
+(composite number - number that can be formed by multiplying 2 numbers).
+Function's parameters:
+number - number to be verified,whether it is prime number of not.}
+Function IsPrimeNumber(number:integer):boolean;
+var k, ostatok:integer;
 begin
-	i := 1; //first character in the string
-	WordCount := 0; //no words found yet
-	while (i<=length(s)) do begin //while not the end of the string 
-		if s[i]=' ' then begin //looking for word to start
-			inc(i);			
-		end else begin //word found
-			FirstLetterPosition := i; //character is the first letter of word
-			while s[i]<>' ' do begin //while word is not ended
-				inc(i);				//check characters till the end of the word
-			end;      //when no more characters
-			inc(WordCount); //end of the word
-			words[WordCount] := copy(s, FirstLetterPosition, i - FirstLetterPosition);
-		end;		//stores words in the array WordArray
+	k:=1;
+	repeat
+		inc(k);
+		ostatok:=number mod k;
+		if ostatok=0 then begin
+			break;
+		end;
+	until k>sqrt(number);
+	if (ostatok>0) then begin
+		IsPrimeNumber:=true;
 	end;
-end;	
-
-{
-Procedure EqualWordCounter counts number of words, that appear in the
-string more than 1 time.
-}
-
-//words - global variable, used to get access to words in the array
-//WordCount - global variable, used to get number of words in the
-//			string array. Procedure EqualWordCounter goes through
-//			this number to find equal elements
-//i, j, a,times, count, RepeatedWordCounter - local counters, used
-//			to go through the elements in the array of words
-
-procedure EqualWordCounter(words:WordArray; WordCount:integer);
-type NonUniqueWords= array[1 .. 500] of string;
-var i, j, a,times, count, RepeatedWordCounter:integer;
-RepeatedWord:NonUniqueWords;
-NoRepeating:boolean;
-begin
-	i:=1; //first element in the array of words
-	count:=WordCount;
-	RepeatedWordCounter:=0;
-	while i<=(Count) do begin
-		NoRepeating:=true;
-		j:=i+1;
-		times:=1;
-		for a:=0 to RepeatedWordCounter do begin
-			if words[i]=RepeatedWord[a] then begin
-				NoRepeating:=false;			//checking for duplicate elements
-			end;
-		end;
-		if NoRepeating=true then begin
-			while j<=Count do begin
-				if words[i]=words[j] then begin
-					inc(times);//counts how many times word repeated
-				end;
-				inc(j);
-			end;
-		end;
-		if times>1 then begin //store repeating word in the array
-			inc(RepeatedWordCounter); 
-			RepeatedWord[RepeatedWordCounter]:=words[i];
-			writeln('WORD:   ', words[i],'   ', times,' times');
-		end;
-		inc(i);
+	if ostatok=0 then begin
+		IsPrimeNumber:=false;
+	end;
+		if (number=2) then begin
+		IsPrimeNumber:=true;
 	end;
 end;
 
-//f - file variable
-//s - string variable, used to get string input
-//words - elements of the string array WordArray
 
-var f:text;
-	s:string;
-	words:WordArray;
-	WordCount:integer;
-
+{Boolean function DoesNotRepeat checks the array MyArray for redundant elements.
+Function compares the number with each element in the array. If the number
+is original, function is true.
+i - number of elements in the array MyArray
+A - element from the array MyArray}
+Function DoesNotRepeat(i:integer):boolean;
+var j:integer;
 begin
-	writeln('This program displays words from the text file TextWordList.txt .');
-	writeln('Words, that repeat more than 1 time.');
-	assign(f, 'TextWordList.txt');
-	reset(f);
-	WordCount := 0; 
-	while not eof(f) do begin
-		readln(f, s);
-		GetWords(s, words, WordCount);//finds content in the string
-		EqualWordCounter(words, WordCount);{displays number of each word in the array}
+	DoesNotRepeat:=true;
+	WriteLn('A[i]= ', A[i]);
+	for j:=1 to i do begin
+		WriteLn('A[j]= ',A[j]);
+		if A[i]<>A[j] then begin
+			DoesNotRepeat:=true;
+		end else begin
+			DoesNotRepeat:=false;
+		end;
 	end;
-	close(f);
-end.
+end;
+
+
+{Function NumberOfPrimeDivisors writes all prime divisors of
+manually entered number in a global type array MyArray and counts 
+the number of prime divisors.
+number - number manually entered to be divided into prime numbers
+		if possible
+A - var parameter of a function, because function can change an
+	array (can add prime numbers into an array)}
+Function NumberOfPrimeDivisors(number:integer; var A:MyArray):integer;
+var i, j, k, ostatok, diviation: integer;
+begin
+	NumberOfPrimeDivisors:=0;
+	i:=0;
+	k:=1;
+	for k:=2 to number do begin //check if k is prime
+		if (IsPrimeNumber(k)=true) then begin
+			WriteLn('k= ',k);
+			ostatok:= number mod k;//check if k is a divisor
+			WriteLn('ostatok= ', ostatok);
+			if (ostatok=0) then begin //check if k is divisor
+				diviation:=number div k;
+				inc(i);
+				if (DoesNotRepeat(i)=true) then begin
+					A[i]:=k;
+					//WriteLn('A[i]= ', A[i]);
+					NumberOfPrimeDivisors:=NumberOfPrimeDivisors+1;
+					//WriteLn('NumberOfPrimeDivisors= ', NumberOfPrimeDivisors);
+				end;
+			end;
+		end;
+	end;
+end;
+
+
+var number:integer;
+Begin
+	WriteLn('Please enter the number greater than 1:');
+	Read(number);
+	if IsPrimeNumber(number)=true then begin
+		WriteLn('The number you entered is prime.');
+	end else begin
+		WriteLn('The number of prime divisors is: ', NumberOfPrimeDivisors(number, A));
+	end;
+End.
+
